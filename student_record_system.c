3ac11308328d941f2959cdf63bf2ea43ede343d2
afsheen-nadaf <afsheensameerahmednadafbtech24@rvu.edu.in>
2025-05-14 10:33:44 +0530

    Update student_record_system.c

--- student_record_system.c
+++ student_record_system.c
@@ -9,6 +9,7 @@ typedef struct {
     int id;
     char name[NAME_SIZE];
     int isOccupied;
+    int isDeleted; // New flag
 } Student;
 
 Student hashTable[TABLE_SIZE];
@@ -16,6 +17,7 @@ Student hashTable[TABLE_SIZE];
 void initTable() {
     for (int i = 0; i < TABLE_SIZE; i++) {
         hashTable[i].isOccupied = 0;
+        hashTable[i].isDeleted = 0;
     }
 }
 
@@ -27,7 +29,7 @@ void insertStudent(int id, const char* name) {
     int index = hashFunction(id);
     int startIndex = index;
 
-    while (hashTable[index].isOccupied) {
+    while (hashTable[index].isOccupied && !hashTable[index].isDeleted) {
         if (hashTable[index].id == id) {
             printf("Duplicate ID! Student already exists.\n");
             return;
@@ -42,6 +44,7 @@ void insertStudent(int id, const char* name) {
     hashTable[index].id = id;
     strcpy(hashTable[index].name, name);
     hashTable[index].isOccupied = 1;
+    hashTable[index].isDeleted = 0;
     printf("Student inserted at index %d.\n", index);
 }
 
@@ -49,10 +52,28 @@ void searchStudent(int id) {
     int index = hashFunction(id);
     int startIndex = index;
 
-    while (hashTable[index].isOccupied) {
-        if (hashTable[index].id == id) {
+    while (hashTable[index].isOccupied || hashTable[index].isDeleted) {
+        if (hashTable[index].isOccupied && !hashTable[index].isDeleted && hashTable[index].id == id) {
             printf("Student Found:\n");
             printf("ID: %d, Name: %s\n", hashTable[index].id, hashTable[index].name);
+            return;
+        }
+        index = (index + 1) % TABLE_SIZE;
+        if (index == startIndex) break;
+    }
+
+    printf("Student with ID %d not found.\n", id);
+}
+
+void deleteStudent(int id) {
+    int index = hashFunction(id);
+    int startIndex = index;
+
+    while (hashTable[index].isOccupied || hashTable[index].isDeleted) {
+        if (hashTable[index].isOccupied && !hashTable[index].isDeleted && hashTable[index].id == id) {
+            hashTable[index].isDeleted = 1;
+            hashTable[index].isOccupied = 0;
+            printf("Student with ID %d deleted.\n", id);
             return;
         }
         index = (index + 1) % TABLE_SIZE;
@@ -69,7 +90,7 @@ int main() {
 
     do {
         printf("\n--- Student Record System ---\n");
-        printf("1. Insert Student\n2. Search Student\n3. Exit\n");
+        printf("1. Insert Student\n2. Search Student\n3. Delete Student\n4. Exit\n");
         printf("Enter choice: ");
         scanf("%d", &choice);
         getchar(); 
@@ -92,6 +113,12 @@ int main() {
                 break;
 
             case 3:
+                printf("Enter student ID to delete: ");
+                scanf("%d", &id);
+                deleteStudent(id);
+                break;
+
+            case 4:
                 printf("Exiting...\n");
                 break;
 
@@ -99,7 +126,7 @@ int main() {
                 printf("Invalid choice.\n");
         }
 
-    } while (choice != 3);
+    } while (choice != 4);
 
     return 0;
 }
